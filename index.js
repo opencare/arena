c7d50ce2369ac310defdf6f79c3017a7c7eee67c
hilaldib9 <hilal@opencare.com>
2018-12-18 10:27:34 -0500

    Minor change to trigger build

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const Arena = require('./src/server/app');
-const routes = require('./src/server/views/routes');
+ const routes = require('./src/server/views/routes');
 
 function run(config, listenOpts = {}) {
   const {app, Queues} = Arena();
